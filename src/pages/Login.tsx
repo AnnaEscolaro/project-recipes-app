e3ccf53b95b9ffdb010d86c4d67bab9bd4a7fd5d
Kailany Silva <kailanysilva1909@gmail.com>
2023-08-21 14:19:12 -0300

    feito inputs login e password, e validacao de email e senha

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,35 @@
+import { useLogin } from '../hooks/useLogin';
+
 export default function Login() {
+  const email = useLogin('');
+  const password = useLogin('');
+
+  const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i;
+  console.log(regexEmail.test(email.value));
+  console.log(email.value);
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form>
+      <input
+        value={ email.value }
+        type="email"
+        name="email"
+        data-testid="email-input"
+        onChange={ ({ target }) => email.handleChange(target.value) }
+      />
+      <input
+        type="password"
+        value={ password.value }
+        name="senha"
+        data-testid="password-input"
+        onChange={ ({ target }) => password.handleChange(target.value) }
+      />
+      <button
+        data-testid="login-submit-btn"
+        disabled={ password.value.length <= 6 || !regexEmail.test(email.value) }
+      >
+        Enter
+      </button>
+    </form>
   );
 }
