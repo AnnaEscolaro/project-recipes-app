62462371404227b300ef158c641e73d4d25e3074
IanPedroBorges <ianhudback@gmail.com>
2023-08-22 18:29:13 -0300

    implementando o carosel e o button start recipe

--- src/components/DrinksDetails.tsx
+++ src/components/DrinksDetails.tsx
@@ -1,6 +1,8 @@
-import { Drinks } from '../types/typesApi';
+import { useEffect, useState } from 'react';
+import { Drinks, Meals } from '../types/typesApi';
 
 export default function DrinksDetail({ drink }: { drink: Drinks }) {
+  const [data, setData] = useState<Meals[]>([]);
   const { strDrink, strDrinkThumb, strAlcoholic, strInstructions } = drink;
   const ingredients = Object.entries(drink).reduce(
     (acc: string[], curr: string[]) => {
@@ -21,6 +23,21 @@ export default function DrinksDetail({ drink }: { drink: Drinks }) {
     [],
   );
 
+  useEffect(() => {
+    const recomandationDrink = async () => {
+      try {
+        const response = await fetch(
+          'https://www.themealdb.com/api/json/v1/1/search.php?s=',
+        );
+        const mealsRecomentation = await response.json();
+        setData(mealsRecomentation.meals);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    recomandationDrink();
+  }, []);
+
   return (
     <div>
       <img src={ strDrinkThumb } alt={ strDrink } data-testid="recipe-photo" />
@@ -40,6 +57,30 @@ export default function DrinksDetail({ drink }: { drink: Drinks }) {
         ))}
       </ul>
       <p data-testid="instructions">{strInstructions}</p>
+      <div style={ { display: 'flex', overflowY: 'scroll' } }>
+        {data
+          && data.slice(0, 6).map((meals, index) => (
+            <div
+              key={ meals.strMeal }
+              data-testid={ `${index}-recommendation-card` }
+            >
+              <img
+                src={ meals.strMealThumb }
+                alt={ meals.strMeal }
+                style={ { width: '50vw' } }
+              />
+              <p data-testid={ `${index}-recommendation-title` }>
+                {meals.strMeal}
+              </p>
+            </div>
+          ))}
+      </div>
+      <button
+        data-testid="start-recipe-btn"
+        style={ { position: 'fixed', bottom: 0 } }
+      >
+        Start Recipe
+      </button>
     </div>
   );
 }
--- src/components/MealsDetails.tsx
+++ src/components/MealsDetails.tsx
@@ -1,11 +1,17 @@
-import { Meals } from '../types/typesApi';
+import { useEffect, useState } from 'react';
+import { Drinks, Meals } from '../types/typesApi';
 
 export default function MealsDetails({ meals }: { meals: Meals }) {
+  const [data, setData] = useState<Drinks[]>([]);
   const { strMeal, strMealThumb, strCategory, strYoutube, strInstructions } = meals;
 
   const ingredients = Object.entries(meals).reduce(
     (acc: string[], curr: string[]) => {
-      if (curr[0].includes('strIngredient') && curr[1] !== null) {
+      if (
+        curr[0].includes('strIngredient')
+        && curr[1] !== null
+        && curr[1] !== ''
+      ) {
         acc.push(curr[1]);
       }
       return acc;
@@ -22,9 +28,28 @@ export default function MealsDetails({ meals }: { meals: Meals }) {
     [],
   );
 
+  useEffect(() => {
+    const recomandationMeals = async () => {
+      try {
+        const response = await fetch(
+          'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=',
+        );
+        const drinkRecomantation = await response.json();
+        setData(drinkRecomantation.drinks);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    recomandationMeals();
+  }, []);
   return (
     <div>
-      <img src={ strMealThumb } alt={ strMeal } data-testid="recipe-photo" width="320" />
+      <img
+        src={ strMealThumb }
+        alt={ strMeal }
+        data-testid="recipe-photo"
+        width="320"
+      />
       <h1 data-testid="recipe-title">{strMeal}</h1>
       <h2 data-testid="recipe-category">{strCategory}</h2>
       {ingredients.map((ingredient, index) => (
@@ -51,8 +76,33 @@ export default function MealsDetails({ meals }: { meals: Meals }) {
         gyroscope;
         picture-in-picture;
         web-share"
+        data-testid="video"
         allowFullScreen
       />
+      <div style={ { display: 'flex', overflowY: 'scroll' } }>
+        {data
+          && data.slice(0, 6).map((drink, index) => (
+            <div
+              key={ drink.strDrink }
+              data-testid={ `${index}-recommendation-card` }
+            >
+              <img
+                src={ drink.strDrinkThumb }
+                alt={ drink.strDrink }
+                style={ { width: '50vw' } }
+              />
+              <p data-testid={ `${index}-recommendation-title` }>
+                {drink.strDrink}
+              </p>
+            </div>
+          ))}
+      </div>
+      <button
+        data-testid="start-recipe-btn"
+        style={ { position: 'fixed', bottom: 0 } }
+      >
+        Start Recipe
+      </button>
     </div>
   );
 }
