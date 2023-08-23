48feeb79204332a9f8f2478628739431529ca3ec
IanPedroBorges <ianhudback@gmail.com>
2023-08-23 15:27:30 -0300

    .

--- src/context/LocalStorageContext/LocalStorageContext.ts
+++ src/context/LocalStorageContext/LocalStorageContext.ts
@@ -1,8 +1,9 @@
 import { createContext } from 'react';
+import { User } from '../../types/typesLocalStorage';
 
 export type LocalStorageType = {
-  user: { email: string }
-  setUser: (value: { email: string }) => void
+  user: User
+  setUser: (value: User) => void
 };
 
 export const LocalStorageContext = createContext({} as LocalStorageType);
