8097fb0694a16bfac0eaeecfdf57cabc78ceaf57
Kailany Silva <kailanysilva1909@gmail.com>
2023-08-22 22:57:26 -0300

    logica de renderizacao do botao feita

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -7,18 +7,27 @@ type RecipesProps = {
 
 function Recipes({ path } : RecipesProps) {
   const [currentValue, setCurrentValue] = useState([]);
+  const [listCategory, setListCategory] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       if (path === 'meals') {
-        const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-        const data = await response.json();
-        setCurrentValue(data.meals);
+        const responseMeals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+        const dataMeals = await responseMeals.json();
+        setCurrentValue(dataMeals.meals);
+
+        const responseCategoryMeals = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+        const dataCategoryMeals = await responseCategoryMeals.json();
+        setListCategory(dataCategoryMeals.meals);
       }
       if (path === 'drinks') {
-        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
-        const data = await response.json();
-        setCurrentValue(data.drinks);
+        const responseDrinks = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+        const dataDrinks = await responseDrinks.json();
+        setCurrentValue(dataDrinks.drinks);
+
+        const responseCategoryDrinks = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+        const dataCategoryDrinks = await responseCategoryDrinks.json();
+        setListCategory(dataCategoryDrinks.drinks);
       }
     };
     fetchData();
@@ -27,13 +36,25 @@ function Recipes({ path } : RecipesProps) {
   return (
     <>
       <h1>Recipes</h1>
-      {currentValue && (
-        currentValue.slice(0, 12).map((current, index) => {
-          return (
+      <div>
+        {listCategory && (
+          listCategory.slice(0, 5).map((category: { strCategory: string }, index) => (
+            <button
+              key={ index }
+              data-testid={ `${category.strCategory}-category-filter` }
+            >
+              {category.strCategory}
+            </button>
+          ))
+        )}
+      </div>
+      <main>
+        {currentValue && (
+          currentValue.slice(0, 12).map((current, index) => (
             <CardRecipes key={ index } data={ current } type={ path } index={ index } />
-          );
-        })
-      )}
+          ))
+        )}
+      </main>
     </>
   );
 }
