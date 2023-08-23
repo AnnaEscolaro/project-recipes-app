ebcafd5eb4206bbde6ed7937aad7469e3e59bd3d
Anna Escolaro <annaescolaro@hotmail.com>
2023-08-23 18:50:09 -0300

    testes searchbar

--- src/tests/SearchBarDrinks.test.tsx
+++ src/tests/SearchBarDrinks.test.tsx
@@ -1,19 +1,20 @@
 import { screen } from '@testing-library/dom';
 import { vi } from 'vitest';
 import { renderWithRouter } from './Helpers/renderWithRouter';
-import { MockFetchDrinks } from './Mocks/MockFetchDrinks';
+import { MockFetchDrinksByIngredient } from './Mocks/mockDrinksByIngredient';
+import { mockFetchDrinksByName } from './Mocks/mockDrinksByName';
+import { mockFetchDrinksByFirstLetter } from './Mocks/mockDrinksByFirstLetter';
 import App from '../App';
 
 describe('Testando o componente SearchBar', () => {
   const searchInput = 'search-input';
-  //   const expectedDrink = 'Munich Mule';
 
-  beforeEach(() => {
-    window.alert = vi.fn(() => {});
-    global.fetch = vi.fn().mockResolvedValue({
-      json: async () => MockFetchDrinks,
-    });
-  });
+  //   beforeEach(() => {
+  //     window.alert = vi.fn(() => {});
+  //     global.fetch = vi.fn().mockResolvedValue({
+  //       json: async () => MockFetchDrinks,
+  //     });
+  //   });
 
   afterEach(() => {
     vi.clearAllMocks();
@@ -31,55 +32,72 @@ describe('Testando o componente SearchBar', () => {
     expect(searchButton).toBeInTheDocument();
   });
 
-  //   test('Se o filtro ingredient funciona corretamente na tela drinks', async () => {
-  //     const { user } = renderWithRouter(<App />, { route: '/drinks' });
-  //     const ingredient = screen.getByLabelText(/Ingredient/i);
-  //     const searchButtonHeader = screen.getByTestId('btn-Click');
-  //     const searchButtonBar = screen.getByRole('button', { name: /Search/i });
+  test('Se o filtro ingredient funciona corretamente na tela drinks', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => MockFetchDrinksByIngredient,
+    });
 
-  //     await user.click(searchButtonHeader);
-  //     const textInput = screen.getByTestId(searchInput);
-  //     await user.type(textInput, 'Gin');
-  //     await user.click(ingredient);
-  //     await user.click(searchButtonBar);
-  //     const drink = await screen.findByText(expectedDrink);
+    const { user } = renderWithRouter(<App />, { route: '/drinks' });
+    const ingredient = screen.getByLabelText(/Ingredient/i);
+    const searchButtonHeader = screen.getByTestId('btn-Click');
+    const searchButtonBar = screen.getByRole('button', { name: /Search/i });
 
-  //     expect(drink).toBeInTheDocument();
-  //   });
+    await user.click(searchButtonHeader);
+    const textInput = screen.getByTestId(searchInput);
+    await user.type(textInput, 'Gin');
+    await user.click(ingredient);
+    await user.click(searchButtonBar);
+    const drink = await screen.findByText('Abbey Cocktail');
 
-  //   test('Se o filtro name funciona corretamente na tela drinks', async () => {
-  //     const { user } = renderWithRouter(<App />, { route: '/drinks' });
-  //     const name = screen.getByLabelText(/Name/i);
-  //     const searchButtonHeader = screen.getByTestId('btn-Click');
-  //     const searchButtonBar = screen.getByRole('button', { name: /Search/i });
+    expect(drink).toBeInTheDocument();
+  });
 
-  //     await user.click(searchButtonHeader);
-  //     const textInput = screen.getByTestId(searchInput);
-  //     await user.type(textInput, 'munich');
-  //     await user.click(name);
-  //     await user.click(searchButtonBar);
+  test('Se o filtro name funciona corretamente na tela drinks', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => mockFetchDrinksByName,
+    });
 
-  //     const drink = await screen.findByText(expectedDrink);
-  //     expect(drink).toBeInTheDocument();
-  //   });
+    const { user } = renderWithRouter(<App />, { route: '/drinks' });
+    const name = screen.getByLabelText(/Name/i);
+    const searchButtonHeader = screen.getByTestId('btn-Click');
+    const searchButtonBar = screen.getByRole('button', { name: /Search/i });
 
-  //   test('Se o filtro firstletter funciona corretamente na tela drinks', async () => {
-  //     const { user } = renderWithRouter(<App />, { route: '/drinks' });
-  //     const firstLetter = screen.getByLabelText(/First-Letter/i);
-  //     const searchButtonHeader = screen.getByTestId('btn-Click');
-  //     const searchButtonBar = screen.getByRole('button', { name: /Search/i });
+    await user.click(searchButtonHeader);
+    const textInput = screen.getByTestId(searchInput);
+    await user.type(textInput, 'mule');
+    await user.click(name);
+    await user.click(searchButtonBar);
 
-  //     await user.click(searchButtonHeader);
-  //     const textInput = screen.getByTestId(searchInput);
-  //     await user.type(textInput, 'm');
-  //     await user.click(firstLetter);
-  //     await user.click(searchButtonBar);
+    const drink = await screen.findByText('Moscow Mule');
+    expect(drink).toBeInTheDocument();
+  });
 
-  //     const drink = await screen.findByText(expectedDrink);
-  //     expect(drink).toBeInTheDocument();
-  //   });
+  test('Se o filtro firstletter funciona corretamente na tela drinks', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => mockFetchDrinksByFirstLetter,
+    });
+
+    const { user } = renderWithRouter(<App />, { route: '/drinks' });
+    const firstLetter = screen.getByLabelText(/First-Letter/i);
+    const searchButtonHeader = screen.getByTestId('btn-Click');
+    const searchButtonBar = screen.getByRole('button', { name: /Search/i });
+
+    await user.click(searchButtonHeader);
+    const textInput = screen.getByTestId(searchInput);
+    await user.type(textInput, 'a');
+    await user.click(firstLetter);
+    await user.click(searchButtonBar);
+
+    const drink = await screen.findByText('Ace');
+    expect(drink).toBeInTheDocument();
+  });
 
   test('Se aparece um alerta na tela de drinks caso sejam digitadas mais de uma letra no filtro firstletter', async () => {
+    window.alert = vi.fn(() => {});
+    // global.fetch = vi.fn().mockResolvedValue({
+    //   json: async () => null,
+    // });
+
     const { user } = renderWithRouter(<App />, { route: '/drinks' });
     const firstLetter = screen.getByLabelText(/First-Letter/i);
     const searchButtonHeader = screen.getByTestId('btn-Click');
@@ -98,7 +116,7 @@ describe(('Testando o alerta de receita não encontrada'), () => {
   test('Se aparece um alerta na tela de drinks caso a receita não exista', async () => {
     window.alert = vi.fn(() => {});
     global.fetch = vi.fn().mockResolvedValue({
-      json: async () => null,
+      json: async () => ({ drinks: null }),
     });
 
     const { user } = renderWithRouter(<App />, { route: '/drinks' });
