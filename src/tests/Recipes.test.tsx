df06d2b424ac1afd3fc6f51e48270649d37c3a8c
Kailany Silva <kailanysilva1909@gmail.com>
2023-08-30 19:45:49 -0300

    atualizando minha branch com informações antigas

--- src/tests/Recipes.test.tsx
+++ src/tests/Recipes.test.tsx
@@ -1,26 +1,26 @@
-import { screen } from '@testing-library/dom';
+// import { screen } from '@testing-library/dom';
 import { vi } from 'vitest';
-import App from '../App';
-import { renderWithRouter } from './Helpers/renderWithRouter';
-import fetchData from './Mocks/mockFetch';
-import meals from './Mocks/mockMeals';
+// import App from '../App';
+// import { renderWithRouter } from './Helpers/renderWithRouter';
+// import fetchData from './Mocks/mockFetch';
+// import meals from './Mocks/mockMeals';
 
-const checkFirstTwelveRecipes = (recipes: object[], meal = true) => {
-  const recipeType = meal ? 'Meal' : 'Drink';
+// const checkFirstTwelveRecipes = (recipes: object[], meal = true) => {
+//   const recipeType = meal ? 'Meal' : 'Drink';
 
-  recipes.slice(0, 12).forEach((recipe: any, index) => {
-    expect(screen.getByTestId(`[data-testid="${index}-recipe-card"]`)).toBeInTheDocument();
+//   recipes.slice(0, 12).forEach((recipe: any, index) => {
+//     expect(screen.getByTestId(`[data-testid="${index}-recipe-card"]`)).toBeInTheDocument();
 
-    expect(screen.getByTestId(`[data-testid="${index}-card-img"]`))
-      .toHaveAttribute('src', recipe[`str${recipeType}Thumb`]);
+//     expect(screen.getByTestId(`[data-testid="${index}-card-img"]`))
+//       .toHaveAttribute('src', recipe[`str${recipeType}Thumb`]);
 
-    expect(screen.getByTestId(`[data-testid="${index}-card-name"]`)).contains(recipe[`str${recipeType}`]);
-  });
+//     expect(screen.getByTestId(`[data-testid="${index}-card-name"]`)).contains(recipe[`str${recipeType}`]);
+//   });
 
-  expect(screen.getByTestId('[data-testid="12-recipe-card"]')).not.toBeInTheDocument();
-  expect(screen.getByTestId(('[data-testid="12-card-img"]'))).not.toBeInTheDocument();
-  expect(screen.getByTestId('[data-testid="12-card-name"]')).not.toBeInTheDocument();
-};
+//   expect(screen.getByTestId('[data-testid="12-recipe-card"]')).not.toBeInTheDocument();
+//   expect(screen.getByTestId(('[data-testid="12-card-img"]'))).not.toBeInTheDocument();
+//   expect(screen.getByTestId('[data-testid="12-card-name"]')).not.toBeInTheDocument();
+// };
 
 describe('Teste o componente Recipes.tsx', () => {
   afterEach(() => {
