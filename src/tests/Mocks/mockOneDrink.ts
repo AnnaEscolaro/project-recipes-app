932d633ee0f15ae8c728f42538a5775ce5c6648d
Kailany Silva <kailanysilva1909@gmail.com>
2023-08-30 18:30:48 -0300

    implementando mocks

--- src/tests/Mocks/mockOneDrink.ts
+++ src/tests/Mocks/mockOneDrink.ts
@@ -1,4 +1,4 @@
-export const mockFetchOneDrink = {
+const mockOneDrink = {
   drinks: [
     {
       idDrink: '11009',
@@ -55,3 +55,5 @@ export const mockFetchOneDrink = {
     },
   ],
 };
+
+export default mockOneDrink;
---
+++ src/tests/Recipes.test.tsx
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/dom';
+import { vi } from 'vitest';
+import App from '../App';
+import { renderWithRouter } from './Helpers/renderWithRouter';
+import fetchData from './Mocks/mockFetch';
+import meals from './Mocks/mockMeals';
+
+const checkFirstTwelveRecipes = (recipes: object[], meal = true) => {
+  const recipeType = meal ? 'Meal' : 'Drink';
+
+  recipes.slice(0, 12).forEach((recipe: any, index) => {
+    expect(screen.getByTestId(`[data-testid="${index}-recipe-card"]`)).toBeInTheDocument();
+
+    expect(screen.getByTestId(`[data-testid="${index}-card-img"]`))
+      .toHaveAttribute('src', recipe[`str${recipeType}Thumb`]);
+
+    expect(screen.getByTestId(`[data-testid="${index}-card-name"]`)).contains(recipe[`str${recipeType}`]);
+  });
+
+  expect(screen.getByTestId('[data-testid="12-recipe-card"]')).not.toBeInTheDocument();
+  expect(screen.getByTestId(('[data-testid="12-card-img"]'))).not.toBeInTheDocument();
+  expect(screen.getByTestId('[data-testid="12-card-name"]')).not.toBeInTheDocument();
+};
+
+describe('Teste o componente Recipes.tsx', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  test('Verifica se na rota "/meals" carrega as 12 primeiras receitas', () => {
+    renderWithRouter(<App />, { route: '/meals' });
+
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (fetchData),
+    });
+
+    checkFirstTwelveRecipes(meals.meals);
+  });
+});
