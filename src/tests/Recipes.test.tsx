3f9fd2bceb8a6c09c06dba9ef67101f93c93b9cd
Kailany Silva <kailanysilva1909@gmail.com>
2023-08-31 17:46:21 -0300

    fiz os testes do recipes aparentemente

--- src/tests/Recipes.test.tsx
+++ src/tests/Recipes.test.tsx
@@ -4,6 +4,7 @@ import App from '../App';
 import { renderWithRouter } from './Helpers/renderWithRouter';
 import fetchData from './Mocks/mockFetch';
 import meals from './Mocks/mockMeals';
+import drinks from './Mocks/mockDrinks';
 
 const checkFirstTwelveRecipes = (recipes: object[], meal = true) => {
   // const recipeType = meal ? 'Meal' : 'Drink';
@@ -47,5 +48,45 @@ describe('Teste o componente Recipes.tsx', () => {
     const btnAll = screen.getByRole('button', { name: /all/i });
 
     await user.click(btnAll);
+
+    checkFirstTwelveRecipes(meals.meals);
+
+    const btnChicken = screen.getByRole('button', { name: /chicken/i });
+    expect(btnChicken).toBeInTheDocument();
+
+    await user.click(btnChicken);
+    await user.click(btnChicken);
+
+    checkFirstTwelveRecipes(meals.meals);
+  });
+  test('Verifica se na rota "/drinks" carrega as categorias certas', async () => {
+    const { user } = renderWithRouter(<App />, { route: '/drinks' });
+
+    await waitFor(() => {
+      expect(screen.getByTestId('Ordinary Drink-category-filter')).toBeInTheDocument();
+    }, {
+      timeout: 1500,
+    });
+
+    checkFirstTwelveRecipes(drinks.drinks);
+
+    const btnOrdinaryDrink = screen.getByRole('button', { name: /ordinary drink/i });
+    expect(btnOrdinaryDrink).toBeInTheDocument();
+
+    await user.click(btnOrdinaryDrink);
+
+    const btnAll = screen.getByRole('button', { name: /all/i });
+
+    await user.click(btnAll);
+
+    checkFirstTwelveRecipes(drinks.drinks);
+
+    const btnCocktail = screen.getByRole('button', { name: /cocktail/i });
+    expect(btnCocktail).toBeInTheDocument();
+
+    await user.click(btnCocktail);
+    await user.click(btnCocktail);
+
+    checkFirstTwelveRecipes(drinks.drinks);
   });
 });
