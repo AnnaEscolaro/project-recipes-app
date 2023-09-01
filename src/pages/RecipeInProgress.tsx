e0ac6b607229b77ddab4747bf758b392e98dc4ab
Lucas Gabriel <122024167+lucazgbrl@users.noreply.github.com>
2023-09-01 15:23:47 -0300

    some fixes

--- src/pages/RecipeInProgress.tsx
+++ src/pages/RecipeInProgress.tsx
@@ -3,7 +3,6 @@ import RecipeDetails from './RecipeDetails';
 export default function RecipeInProgress() {
   return (
     <div>
-      <h1>Recipe In Progress</h1>
       <RecipeDetails />
     </div>
   );
